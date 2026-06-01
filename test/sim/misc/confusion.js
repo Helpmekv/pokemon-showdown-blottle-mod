eebc1588d9ed82afe507cb5b0e8658c824f08471
Helpmekv <149512859+Helpmekv@users.noreply.github.com>
2026-06-01 10:24:34 -0400

    Added funny kys status
    YOUR LIFE IS NOTHING. YOU SERVE ZERO PURPOSE. YOU SHOULD KILL YOURSELF   N O W !

--- test/sim/misc/confusion.js
+++ test/sim/misc/confusion.js
@@ -21,4 +21,21 @@ describe('Confusion', () => {
 		const damage = deoxys.maxhp - deoxys.hp;
 		assert.bounded(damage, [150, 177]);
 	});
+
+describe('Suicidal', () => {
+	afterEach(() => {
+		battle.destroy();
+	});
+
+	it(`should not be affected by modifiers like Huge Power or Life Orb`, () => {
+		battle = common.createBattle({ forceRandomChance: true }, [[
+			{ species: 'Deoxys-Attack', ability: 'hugepower', item: 'lifeorb', moves: ['sleeptalk'] },
+		], [
+			{ species: 'Sableye', ability: 'prankster', moves: ['traumatization'] },
+		]]);
+		battle.makeChoices();
+		const deoxys = battle.p1.active[0];
+		const damage = deoxys.maxhp - deoxys.maxhp;
+		assert.bounded(damage, [150, 177]);
+	});
 });
